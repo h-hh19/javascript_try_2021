c1932f12aea783d7b570c8c7393d827c6b1e7cf7
h-hh19 <53941170+h-hh19@users.noreply.github.com>
2021-04-13 00:06:12 +0900

    console을 잘 보자

--- todo.js
+++ todo.js
@@ -1,9 +1,37 @@
 const toDoform = document.querySelector(".js-toDoForm"),
-    toDoinput = form.querySelector("input"),
-    toDoList = document.querySelector("js-toDoList");
+    toDoinput = toDoform.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = 'toDos';
+
+function paintToDo(text){
+const li = document.createElement("li");
+const delBtn = document.createElement("button");
+delBtn.innerHTML = "❌";
+const span = document.createElement("span");
+span.innerText = text;
+li.appendChild(span);
+li.appendChild(delBtn);
+toDoList.appendChild(li);  
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoinput.value;
+    paintToDo(currentValue);
+    toDoinput.value = "";
+}
+
+    function loadToDos(){
+        const toDos = localStorage.getItem(TODOS_LS);
+        if(TODOS_LS !== null){
+
+        }
+    }
 
     function init(){
-
+        loadToDos();
+        toDoform.addEventListener("submit", handleSubmit)
     }
-    
+
     init();
